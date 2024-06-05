9963a72fa9e0b15b09b2059865d7f796be99c1e3
Jamie Kim <jamiekimtech313@gmail.com>
2024-06-05 18:02:54 -0400

    Added a dotted profile image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,12 @@
 import { Avatar, Box, Button } from '@mui/material';
-
+import profileImage from '../../src/images/dot_profile.jpg';
 const About = () => {
   return (
     <Box sx={{ textAlign: 'center', maxWidth: 900, p: 4, lineHeight: 1.5 }}>
       <h1 id="about">About Jamie</h1>
       <Avatar
         alt="about"
-        src="https://media.licdn.com/dms/image/D5603AQEVnnYhr24RhQ/profile-displayphoto-shrink_200_200/0/1684348809397?e=1711584000&v=beta&t=XK_2rLLmRdUgTzlCy_7G9qyysCzLOW-tU1j_bMl4wl8"
+        src={profileImage}
         sx={{ width: '40%', height: '40%', margin: 'auto', mt: 3 }}
       />
       <br />
